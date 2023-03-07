1448c64e2f6db19b425f2116e47732594a4a8e4a
Malcolm Ryan <malcoln.ryan@mq.edu.au>
2023-03-07 16:40:00 +1100

    Adde comments to matrices.

--- src/comp3170/demos/week3/camera2d/vertex.glsl
+++ src/comp3170/demos/week3/camera2d/vertex.glsl
@@ -2,12 +2,14 @@
 
 in vec2 a_position;	// vertex position as a 2D vector in MODEL
 
-uniform mat4 u_modelMatrix;
-uniform mat4 u_viewMatrix;
-uniform mat4 u_perspectiveMatrix;
+uniform mat4 u_modelMatrix;		// MODEL -> WORLD
+uniform mat4 u_viewMatrix;		// WORLD -> VIEW
+uniform mat4 u_perspectiveMatrix;	// VIEW -> NDC
 
 void main() {
 	vec4 position = vec4(a_position,0,1);
-    gl_Position = u_perspectiveMatrix * u_viewMatrix * u_modelMatrix * position;
+	
+	gl_Position = u_perspectiveMatrix * u_viewMatrix * u_modelMatrix * position;
+		
 }
 
