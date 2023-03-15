e817d5d603656dbe3012d866cf563219b76704c6
Malcolm Ryan <malcoln.ryan@mq.edu.au>
2023-03-15 22:36:16 +1100

    Week 5 Mesh Demo

---
+++ src/comp3170/demos/week5/mesh/MeshDemo.java
@@ -0,0 +1,104 @@
+package comp3170.demos.week5.mesh;
+import static org.lwjgl.glfw.GLFW.*;
+import static org.lwjgl.opengl.GL11.GL_COLOR_BUFFER_BIT;
+import static org.lwjgl.opengl.GL11.glClear;
+import static org.lwjgl.opengl.GL11.glClearColor;
+import static org.lwjgl.opengl.GL11.glViewport;
+
+import comp3170.IWindowListener;
+import comp3170.InputManager;
+import comp3170.OpenGLException;
+import comp3170.SceneObject;
+import comp3170.Window;
+import comp3170.demos.week5.mesh.sceneobjects.SimpleCube;
+import comp3170.demos.week5.mesh.sceneobjects.UVSphere;
+
+public class MeshDemo implements IWindowListener {
+
+	public static final float TAU = (float) (2 * Math.PI);		// https://tauday.com/tau-manifesto
+
+	private Window window;
+	private int screenWidth = 800;
+	private int screenHeight = 800;
+	private InputManager input;
+	private long oldTime;
+	private SceneObject[] meshes;
+	private int currentMesh;
+
+	public MeshDemo() throws OpenGLException {
+		window = new Window("Week 5 mesh demo", screenWidth, screenHeight, this);
+		window.run();
+	}
+	
+	@Override
+	public void init() {
+		glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+		
+		meshes = new SceneObject[] {
+			new SimpleCube(),
+			new UVSphere(),
+		};
+		currentMesh = 0;
+		
+		for (int i = 0; i < meshes.length; i++) {
+			meshes[i].getMatrix().rotateX(TAU/8).rotateZ(TAU/8);			
+		}
+		
+	    // initialise oldTime
+		input = new InputManager(window);
+	    oldTime = System.currentTimeMillis();
+
+	}
+
+	private final float ROTATION_SPEED = TAU / 8;
+
+	private void update() {
+		long time = System.currentTimeMillis();
+		float deltaTime = (time - oldTime) / 1000f;
+		oldTime = time;
+
+		if (input.isKeyDown(GLFW_KEY_LEFT)) {
+			meshes[currentMesh].getMatrix().rotateY(ROTATION_SPEED * deltaTime);			
+		}
+		if (input.isKeyDown(GLFW_KEY_RIGHT)) {
+			meshes[currentMesh].getMatrix().rotateY(-ROTATION_SPEED * deltaTime);			
+		}
+		if (input.isKeyDown(GLFW_KEY_UP)) {
+			meshes[currentMesh].getMatrix().rotateX(ROTATION_SPEED * deltaTime);			
+		}
+		if (input.isKeyDown(GLFW_KEY_DOWN)) {
+			meshes[currentMesh].getMatrix().rotateX(-ROTATION_SPEED * deltaTime);			
+		}
+		
+		if (input.wasKeyPressed(GLFW_KEY_SPACE)) {
+			currentMesh = (currentMesh + 1) % meshes.length;
+		}
+
+		input.clear();
+	}
+
+	@Override
+	public void draw() {
+		update();
+
+		glClear(GL_COLOR_BUFFER_BIT);		
+		meshes[currentMesh].draw();
+	}
+
+	@Override
+	public void resize(int width, int height) {
+		screenWidth = width;
+		screenHeight = height;
+		glViewport(0, 0, width, height);
+	}
+
+	@Override
+	public void close() {
+		// TODO Auto-generated method stub
+
+	}
+
+	public static void main(String[] args) throws OpenGLException {
+		new MeshDemo();
+	}
+}
---
+++ src/comp3170/demos/week5/mesh/sceneobjects/UVSphere.java
@@ -0,0 +1,117 @@
+package comp3170.demos.week5.mesh.sceneobjects;
+
+import static org.lwjgl.opengl.GL11.GL_FRONT_AND_BACK;
+import static org.lwjgl.opengl.GL11.GL_LINE;
+import static org.lwjgl.opengl.GL11.GL_TRIANGLES;
+import static org.lwjgl.opengl.GL11.GL_UNSIGNED_INT;
+import static org.lwjgl.opengl.GL11.glDrawElements;
+import static org.lwjgl.opengl.GL11.glPolygonMode;
+import static org.lwjgl.opengl.GL15.GL_ELEMENT_ARRAY_BUFFER;
+import static org.lwjgl.opengl.GL15.glBindBuffer;
+
+import org.joml.Matrix4f;
+import org.joml.Vector4f;
+
+import comp3170.GLBuffers;
+import comp3170.SceneObject;
+import comp3170.Shader;
+import comp3170.demos.week5.mesh.shaders.ShaderLibrary;
+import static comp3170.demos.week5.mesh.MeshDemo.TAU;
+
+public class UVSphere extends SceneObject {
+
+	private static final String VERTEX_SHADER = "simpleVertex.glsl";
+	private static final String FRAGMENT_SHADER = "simpleFragment.glsl";
+	private static final int NSEGMENTS = 20;
+	
+	private Shader shader;
+	private Vector4f[] vertices;
+	private int vertexBuffer;
+	private int[] indices;
+	private int indexBuffer;
+	private float[] colour = {1f, 1f, 1f, 1f};
+
+	public UVSphere() {
+		shader = ShaderLibrary.compileShader(VERTEX_SHADER, FRAGMENT_SHADER);
+		int width = 2 * NSEGMENTS;
+		int height = NSEGMENTS;
+		
+		createVertexBuffer(width, height);
+	    createIndexBuffer(width, height);
+
+	}
+
+	private void createVertexBuffer(int width, int height) {
+		//
+		// Create a (2n+1) * (n+1) grid of points in polar space
+		//
+
+		vertices = new Vector4f[(width +1) * (height+1)];
+		
+		Matrix4f rotateY = new Matrix4f();
+		Matrix4f rotateX = new Matrix4f();
+		Matrix4f translateY = new Matrix4f();
+
+		int k = 0;
+		for (int i = 0; i <= width; i++) {
+			float heading = i * TAU / width;	// from 0 to TAU
+			rotateY.rotationY(heading);
+			
+			for (int j = 0; j <= height; j++) {
+				float t = j * 2f / height - 1;
+				float pitch = (TAU /4) * t; // from -TAU/4 to TAU/4
+				rotateX.rotationX(pitch);
+				translateY.translation(0, t, 0);
+				
+				vertices[k] = new Vector4f(0, 0, 1, 1);	// unit vector in z direction
+				vertices[k].mul(rotateX).mul(rotateY);  // vk = Ry * Rx * v;
+				k++;
+			}			
+		}
+		
+	    vertexBuffer = GLBuffers.createBuffer(vertices);
+	}
+
+	
+	private void createIndexBuffer(int width, int height) {
+		// Each quad looks like
+	    //
+	    // k+1 +--+ k + h + 2
+	    //     |\ |
+	    //     | \|
+	    //   k +--+ k + h + 1
+	    
+	    indices = new int[12 * width * height]; // 2 tris * 3  verts * width height
+
+	    int n = 0;
+		for (int i = 0; i < width; i++) {		// there is no quad for the right-hand column
+			for (int j = 0; j < height; j++) { 	// or the top row
+				int k = i * (height+1) + j;
+				
+				indices[n++] = k;
+				indices[n++] = k + height + 1;
+				indices[n++] = k + 1;
+
+				indices[n++] = k + height + 2;
+				indices[n++] = k + 1;
+				indices[n++] = k + height + 1;
+			}			
+		}
+	    
+	    indexBuffer = GLBuffers.createIndexBuffer(indices);
+	}
+
+	
+	@Override
+	protected void drawSelf(Matrix4f modelMatrix) {
+		shader.enable();
+		shader.setUniform("u_mvpMatrix", modelMatrix);
+	    shader.setAttribute("a_position", vertexBuffer);
+	    shader.setUniform("u_colour", colour);	    
+	    
+		glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, indexBuffer);
+		glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+		glDrawElements(GL_TRIANGLES, indices.length, GL_UNSIGNED_INT, 0);
+	}
+
+}
