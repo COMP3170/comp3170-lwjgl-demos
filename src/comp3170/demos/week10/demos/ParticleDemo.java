aab25424e388141ecb376c897cef6848560853bf
Malcolm Ryan <malcoln.ryan@mq.edu.au>
2023-05-03 15:29:16 +1000

    Started particles demo.

---
+++ src/comp3170/demos/week10/demos/ParticleDemo.java
@@ -0,0 +1,35 @@
+package comp3170.demos.week10.demos;
+
+import comp3170.IWindowListener;
+
+public class ParticleDemo implements IWindowListener {
+
+	public ParticleDemo() {
+		
+	}
+	
+	@Override
+	public void init() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void draw() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void resize(int width, int height) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void close() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
