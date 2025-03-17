1462ffdd861860273a59f45fa9a5220526c84eec
Malcolm Ryan <malcolm.ryan@mq.edu.au>
2025-03-17 14:30:52 +1100

    New script to show LWJGL version.

---
+++ src/comp3170/demos/week1/ShowVersion.java
@@ -0,0 +1,11 @@
+package comp3170.demos.week1;
+
+import org.lwjgl.Version;
+
+public class ShowVersion {
+
+	public static void main(String args[]) {
+		
+		System.out.println(Version.getVersion());
+	}
+}
